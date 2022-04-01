13ca0c2d418945ec1dbc8f8e1913e6a51493aad3
Harsh <harsh.here17@gmail.com>
2022-04-01 23:52:57 +0530

    primary button create

---
+++ lib/widgets/buttons.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Buttons {
+  final ThemeData _theme;
+  Buttons(this._theme);
+
+  Widget primaryButton(onPress, String label) => ElevatedButton(
+      onPressed: onPress,
+      style: ElevatedButton.styleFrom(
+          elevation: 10,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          primary: _theme.primaryColor,
+          textStyle: const TextStyle(color: Colors.white, fontSize: 20)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Text(
+          label,
+          style: const TextStyle(fontSize: 20),
+        ),
+      ));
+}
