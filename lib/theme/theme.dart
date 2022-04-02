5490d05553be9a15ae7c2cbdb272029bbe4f7155
Harsh <harsh.here17@gmail.com>
2022-04-02 14:07:04 +0530

    theme colors update

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -7,9 +7,15 @@ class CustomTheme {
         iconTheme: IconThemeData(color: textColor),
         textTheme: TextTheme(
           headline1: TextStyle(color: textColor),
-          headline2: TextStyle(color: subTextColor),
+          headline2: TextStyle(color: textColor),
           bodyText1: TextStyle(color: textColor),
-          bodyText2: TextStyle(color: subTextColor),
+          bodyText2: TextStyle(color: textColor),
+        ),
+        appBarTheme: AppBarTheme(
+          color: primaryColor,
+          textTheme: TextTheme(
+            headline1: TextStyle(color: Colors.white),
+          ),
         ),
         primaryColor: primaryColor,
         backgroundColor: secondaryColor,
